c8e1955aab8593d56f9f26b327406a1afa564112
Pekka Lammi <haloo@pekkalammi.com>
2024-02-15 15:48:15 +0200

    more filtering

--- js2/scripts/filter.js
+++ js2/scripts/filter.js
@@ -7,60 +7,56 @@ const reset = document.querySelector("#filter-reset");
 const boxes = Array.from(document.querySelectorAll(".project"));
 
 // in order to map over multiple elements we need to convert HTMLNodelist to Array with Array.from function
-const aBoxes = Array.from(document.querySelectorAll(".a"));
-const bBoxes = Array.from(document.querySelectorAll(".b"));
-const cBoxes = Array.from(document.querySelectorAll(".c"));
+const aBoxes = Array.from(document.querySelectorAll(".project-wrapper .a"));
+const bBoxes = Array.from(document.querySelectorAll(".project-wrapper .b"));
+const cBoxes = Array.from(document.querySelectorAll(".project-wrapper .c"));
+const allBoxes = [...aBoxes, ...bBoxes, ...cBoxes];
 
 // event listener to be triggered after clicking button
 buttonA.addEventListener("click", () => {
-  console.log("click A");
-  console.log(aBoxes);
+  allBoxes.map((box) => {
+    box.style.display = "block";
+    box.style.opacity = "1";
+  });
 
   // make width of b and c boxes 0 and hide them after 500ms
 
   const hideBoxes = [...bBoxes, ...cBoxes];
 
   hideBoxes.map((box) => {
-    box.style.width = "0";
-  });
-
-  setTimeout(() => {
-    hideBoxes.map((box) => {
-      box.style.display = "none";
-    });
-  }, 500);
-
-  // and after that show only A
-  aBoxes.map((box) => {
-    box.style.display = "block";
+    box.style.display = "none";
   });
 });
 
 // event listener to hide all boxes and then show B boxes
 buttonB.addEventListener("click", () => {
-  console.log("click B");
-
-  // hide everything
-  boxes.map((box) => {
-    box.style.display = "none";
+  allBoxes.map((box) => {
+    box.style.display = "block";
+    box.style.opacity = "1";
   });
 
-  // and after that show only B
-  bBoxes.map((box) => {
-    box.style.display = "block";
+  // make width of a and c boxes 0 and hide them after 500ms
+
+  const hideBoxes = [...aBoxes, ...cBoxes];
+
+  hideBoxes.map((box) => {
+    box.style.display = "none";
   });
 });
 
 // event listener to hide all boxes and then show C boxes
 buttonC.addEventListener("click", () => {
-  console.log("click C");
-
-  boxes.map((box) => {
-    box.style.display = "none";
+  allBoxes.map((box) => {
+    box.style.display = "block";
+    box.style.opacity = "1";
   });
 
-  cBoxes.map((box) => {
-    box.style.display = "block";
+  // make width of a and b boxes 0 and hide them after 500ms
+
+  const hideBoxes = [...aBoxes, ...bBoxes];
+
+  hideBoxes.map((box) => {
+    box.style.display = "none";
   });
 });
 
