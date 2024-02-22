9c12ae5b21e63d5313bd87a69675b08bc7e9ccd9
jasseahokas <jasse@nacu.studio>
2024-02-22 17:01:18 +0200

    Mobiilinavigaation linkit loppuun

--- js2/scripts/mobileMenu.js
+++ js2/scripts/mobileMenu.js
@@ -8,3 +8,14 @@ burgerButton.addEventListener("click", () => {
     burgerButton.classList.toggle("burger-open");
     nav.classList.toggle("nav-open");
 });
+
+// Valitaan kaikki navin linkit
+const navLinks = document.querySelectorAll(".nav-link");
+
+// Lisätään jokaiseen linkkiin tapahtumakuuntelija, joka sulkee menun klikatessa
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        burgerButton.classList.remove("burger-open");
+        nav.classList.remove("nav-open");
+    });
+});
